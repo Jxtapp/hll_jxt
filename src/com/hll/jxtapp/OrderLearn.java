4b4b40beac37b86af529c94b1e51c1aa48e2d0e9
heyi <705098360@qq.com>
2016-06-25 20:20:27 +0800

    第二个页面代码

---
+++ src/com/hll/jxtapp/OrderLearn.java
@@ -0,0 +1,310 @@
+package com.hll.jxtapp;
+
+/**
+ * 预约学车控制Activity
+ * @author heyi
+ * 2016/6/21
+ */
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.app.AlertDialog.Builder;
+public class OrderLearn extends Activity implements OnClickListener{
+
+	private TextView orderLearnMsgTv;
+	//private TextView titleTv;
+	private Spinner exerciceItemChoiceSp;
+	private Spinner exerciceContentChoiceSp;
+	private Spinner exercicePlaceChoiceSp;
+	
+	private ImageButton todayMorningIB;
+	private ImageButton tomorrowMorningIB;
+	private ImageButton theDayAfterTomorrowMorningIB;
+	private ImageButton todayAfternoonIB;
+	private ImageButton tomorrowAfternoonIB;
+	private ImageButton theDayAfterTomorrowAfternoonIB;
+	private ImageButton todayEveningIB;
+	private ImageButton tomorrowEveningIB;
+	private ImageButton theDayAfterTomorrowEveningIB;
+	private ImageButton orderSureButton;
+	private List<String> orderItemList;
+	private List<String> orderContentList;
+	private List<String> orderPlaceList;
+	private ArrayAdapter<String> orderItemAdapter;
+	private ArrayAdapter<String> orderContentAdapter;
+	private ArrayAdapter<String> orderPlaceAdapter;
+	private String userName;
+	private String schoolName;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		setContentView(R.layout.order_learn);  
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title);
+		orderLearnMsgTv=(TextView) findViewById(R.id.id_order_learn_msg);
+		exerciceItemChoiceSp=(Spinner) findViewById(R.id.id_exercice_item_choice);
+		exerciceContentChoiceSp=(Spinner) findViewById(R.id.id_exercice_content_choice);
+		exercicePlaceChoiceSp=(Spinner) findViewById(R.id.id_exercice_place_choice);
+		todayMorningIB=(ImageButton) findViewById(R.id.id_today_morning);
+		tomorrowMorningIB=(ImageButton) findViewById(R.id.id_tomorrow_morning);
+		theDayAfterTomorrowMorningIB=(ImageButton) findViewById(R.id.id_theDayAfterTomorrow_morning);
+		todayAfternoonIB=(ImageButton) findViewById(R.id.id_today_afternoon);
+		tomorrowAfternoonIB=(ImageButton) findViewById(R.id.id_tomorrow_afternoon);
+		theDayAfterTomorrowAfternoonIB=(ImageButton) findViewById(R.id.id_theDayAfterTomorrow_afternoon);
+		todayEveningIB=(ImageButton) findViewById(R.id.id_today_evening);
+		tomorrowEveningIB=(ImageButton) findViewById(R.id.id_tomorrow_evening);
+		theDayAfterTomorrowEveningIB=(ImageButton) findViewById(R.id.id_theDayAfterTomorrow_evening);
+		orderSureButton=(ImageButton) findViewById(R.id.id_order_sure_but);
+		//titleTv=(TextView) findViewById(R.id.id_title);
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		//titleTv.setText("预约学车");
+		userName="李先生(15071285589)";
+		schoolName="交安驾校";
+		orderLearnMsgTv.setText("     尊敬的"+userName+",下面给您带来"+schoolName+"未来三天预约驾校车详情。");
+		//选择训练条件和内容
+		orderItemChoice();
+		initEvent();
+	}
+
+	/*
+	 * 选择训练条件和内容
+	 */
+	private void orderItemChoice() {
+
+		orderItemList=new ArrayList<>();
+		orderContentList=new ArrayList<>();
+		orderPlaceList=new ArrayList<>();
+		
+		orderItemList.add("科目二");
+		orderItemList.add("科目三");
+		
+		orderContentList.add("直行");
+		orderContentList.add("打方向");
+		orderContentList.add("直角转弯");
+		orderContentList.add("S曲线");
+		orderContentList.add("侧方停车");
+		orderContentList.add("上坡起步");
+		orderContentList.add("倒车入库");
+		
+		orderPlaceList.add("天马湖");
+		orderPlaceList.add("东九楼");
+		orderPlaceList.add("赛马场");
+		orderPlaceList.add("杨家湾");
+		
+		orderItemAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,orderItemList);
+		orderContentAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,orderContentList);
+		orderPlaceAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,orderPlaceList);
+		
+		orderItemAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		orderContentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		orderPlaceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		exerciceItemChoiceSp.setAdapter(orderItemAdapter);
+		exerciceContentChoiceSp.setAdapter(orderContentAdapter);
+		exercicePlaceChoiceSp.setAdapter(orderPlaceAdapter);
+
+	}
+
+	private void initEvent() {
+
+		todayMorningIB.setOnClickListener(this);
+		tomorrowMorningIB.setOnClickListener(this);
+		theDayAfterTomorrowMorningIB.setOnClickListener(this);
+		todayAfternoonIB.setOnClickListener(this);
+		tomorrowAfternoonIB.setOnClickListener(this);
+		theDayAfterTomorrowAfternoonIB.setOnClickListener(this);
+		todayEveningIB.setOnClickListener(this);
+		tomorrowEveningIB.setOnClickListener(this);
+		theDayAfterTomorrowEveningIB.setOnClickListener(this);
+		orderSureButton.setOnClickListener(this);
+	}
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.id_today_morning:
+			ImageButton ib0=(ImageButton)todayMorningIB;
+			assert(R.id.id_today_morning==todayMorningIB.getId());
+			Integer integer0 = (Integer) todayMorningIB.getTag();
+			   integer0 = integer0 == null ? 0 : integer0;
+			   switch(integer0) {
+			    case R.drawable.choice_true:
+			    	todayMorningIB.setImageResource(R.drawable.choice_false);
+			    	todayMorningIB.setTag(R.drawable.choice_false);
+			     break;
+			    case R.drawable.choice_false:
+			    default:
+			    	todayMorningIB.setImageResource(R.drawable.choice_true);
+			    	todayMorningIB.setTag(R.drawable.choice_true);
+			     break;
+			}
+			break;
+		case R.id.id_today_afternoon:
+			ImageButton ib1=(ImageButton)todayAfternoonIB;
+			assert(R.id.id_today_morning==todayAfternoonIB.getId());
+			Integer integer1 = (Integer) todayAfternoonIB.getTag();
+			   integer1 = integer1 == null ? 0 : integer1;
+			   switch(integer1) {
+			    case R.drawable.choice_true:
+			    	todayAfternoonIB.setImageResource(R.drawable.choice_false);
+			    	todayAfternoonIB.setTag(R.drawable.choice_false);
+			     break;
+			    case R.drawable.choice_false:
+			    default:
+			    	todayAfternoonIB.setImageResource(R.drawable.choice_true);
+			    	todayAfternoonIB.setTag(R.drawable.choice_true);
+			     break;
+			}
+			break;
+		case R.id.id_today_evening:
+			ImageButton ib2=(ImageButton)todayEveningIB;
+			assert(R.id.id_today_morning==todayEveningIB.getId());
+			Integer integer2 = (Integer) todayEveningIB.getTag();
+			   integer2 = integer2 == null ? 0 : integer2;
+			   switch(integer2) {
+			    case R.drawable.choice_true:
+			    	todayEveningIB.setImageResource(R.drawable.choice_false);
+			    	todayEveningIB.setTag(R.drawable.choice_false);
+			     break;
+			    case R.drawable.choice_false:
+			    default:
+			    	todayEveningIB.setImageResource(R.drawable.choice_true);
+			    	todayEveningIB.setTag(R.drawable.choice_true);
+			     break;
+			}
+			break;
+		case R.id.id_tomorrow_morning:
+			ImageButton ib3=(ImageButton)tomorrowMorningIB;
+			assert(R.id.id_today_morning==tomorrowMorningIB.getId());
+			Integer integer3 = (Integer) tomorrowMorningIB.getTag();
+			   integer3 = integer3 == null ? 0 : integer3;
+			   switch(integer3) {
+			    case R.drawable.choice_true:
+			    	tomorrowMorningIB.setImageResource(R.drawable.choice_false);
+			    	tomorrowMorningIB.setTag(R.drawable.choice_false);
+			     break;
+			    case R.drawable.choice_false:
+			    default:
+			    	tomorrowMorningIB.setImageResource(R.drawable.choice_true);
+			    	tomorrowMorningIB.setTag(R.drawable.choice_true);
+			     break;
+			}
+			break;
+		case R.id.id_tomorrow_afternoon:
+			ImageButton ib4=(ImageButton)tomorrowAfternoonIB;
+			assert(R.id.id_today_morning==tomorrowAfternoonIB.getId());
+			Integer integer4 = (Integer) tomorrowAfternoonIB.getTag();
+			   integer4 = integer4 == null ? 0 : integer4;
+			   switch(integer4) {
+			    case R.drawable.choice_true:
+			    	tomorrowAfternoonIB.setImageResource(R.drawable.choice_false);
+			    	tomorrowAfternoonIB.setTag(R.drawable.choice_false);
+			     break;
+			    case R.drawable.choice_false:
+			    default:
+			    	tomorrowAfternoonIB.setImageResource(R.drawable.choice_true);
+			    	tomorrowAfternoonIB.setTag(R.drawable.choice_true);
+			     break;
+			}
+			break;
+		case R.id.id_tomorrow_evening:
+			ImageButton ib5=(ImageButton)tomorrowEveningIB;
+			assert(R.id.id_today_morning==tomorrowEveningIB.getId());
+			Integer integer5 = (Integer) tomorrowEveningIB.getTag();
+			   integer5 = integer5 == null ? 0 : integer5;
+			   switch(integer5) {
+			    case R.drawable.choice_true:
+			    	tomorrowEveningIB.setImageResource(R.drawable.choice_false);
+			    	tomorrowEveningIB.setTag(R.drawable.choice_false);
+			     break;
+			    case R.drawable.choice_false:
+			    default:
+			    	tomorrowEveningIB.setImageResource(R.drawable.choice_true);
+			    	tomorrowEveningIB.setTag(R.drawable.choice_true);
+			     break;
+			}
+			break;
+		case R.id.id_theDayAfterTomorrow_morning:
+			ImageButton ib6=(ImageButton)theDayAfterTomorrowMorningIB;
+			assert(R.id.id_today_morning==theDayAfterTomorrowMorningIB.getId());
+			Integer integer6 = (Integer) theDayAfterTomorrowMorningIB.getTag();
+			   integer6 = integer6 == null ? 0 : integer6;
+			   switch(integer6) {
+			    case R.drawable.choice_true:
+			    	theDayAfterTomorrowMorningIB.setImageResource(R.drawable.choice_false);
+			    	theDayAfterTomorrowMorningIB.setTag(R.drawable.choice_false);
+			     break;
+			    case R.drawable.choice_false:
+			    default:
+			    	theDayAfterTomorrowMorningIB.setImageResource(R.drawable.choice_true);
+			    	theDayAfterTomorrowMorningIB.setTag(R.drawable.choice_true);
+			     break;
+			}
+			break;
+		case R.id.id_theDayAfterTomorrow_afternoon:
+			ImageButton ib7=(ImageButton)theDayAfterTomorrowAfternoonIB;
+			assert(R.id.id_today_morning==theDayAfterTomorrowAfternoonIB.getId());
+			Integer integer7 = (Integer) theDayAfterTomorrowAfternoonIB.getTag();
+			   integer7 = integer7 == null ? 0 : integer7;
+			   switch(integer7) {
+			    case R.drawable.choice_true:
+			    	theDayAfterTomorrowAfternoonIB.setImageResource(R.drawable.choice_false);
+			    	theDayAfterTomorrowAfternoonIB.setTag(R.drawable.choice_false);
+			     break;
+			    case R.drawable.choice_false:
+			    default:
+			    	theDayAfterTomorrowAfternoonIB.setImageResource(R.drawable.choice_true);
+			    	theDayAfterTomorrowAfternoonIB.setTag(R.drawable.choice_true);
+			     break;
+			}
+			break;
+		case R.id.id_theDayAfterTomorrow_evening:
+			ImageButton ib8=(ImageButton)theDayAfterTomorrowEveningIB;
+			assert(R.id.id_today_morning==theDayAfterTomorrowEveningIB.getId());
+			Integer integer8 = (Integer) theDayAfterTomorrowEveningIB.getTag();
+			   integer8 = integer8 == null ? 0 : integer8;
+			   switch(integer8) {
+			    case R.drawable.choice_true:
+			    	theDayAfterTomorrowEveningIB.setImageResource(R.drawable.choice_false);
+			    	theDayAfterTomorrowEveningIB.setTag(R.drawable.choice_false);
+			     break;
+			    case R.drawable.choice_false:
+			    default:
+			    	theDayAfterTomorrowEveningIB.setImageResource(R.drawable.choice_true);
+			    	theDayAfterTomorrowEveningIB.setTag(R.drawable.choice_true);
+			     break;
+			}
+			break;
+		case R.id.id_order_sure_but:
+			Builder dialog = new AlertDialog.Builder(OrderLearn.this);  
+            dialog.setTitle("温馨提示!您好:");  
+            dialog.setMessage("请认真检查已经选择的时间，方便自己的同时，也方便别人！没问题就点确定吧！");  
+            dialog.setPositiveButton("确定", new DialogInterface.OnClickListener(){  
+                public void onClick(DialogInterface dialog, int which) {  
+                    //好了我成功把Button3的图标掠夺过来  
+                	orderSureButton.setImageDrawable(getResources().getDrawable(R.drawable.order_sure_but));  
+                }  
+            }).create();//创建按钮  
+            dialog.show();
+			break;
+		default:
+			break;
+		}
+	}
+
+}
--- src/com/hll/jxtapp/QueueFragment.java
+++ src/com/hll/jxtapp/QueueFragment.java
@@ -1,4 +1,5 @@
 package com.hll.jxtapp;
+
 /**
  * 驾校通预约排队页面activity
  * @author heyi
@@ -6,6 +7,7 @@ package com.hll.jxtapp;
  */
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 /**
  * app第二个页面，主要实现排队的功能
@@ -14,25 +16,35 @@ import java.util.List;
  */
 import com.hll.adapter.SecPageAdapter;
 import com.hll.entity.SecPageItemBean;
-
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.MarginLayoutParams;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class QueueFragment extends Fragment implements OnClickListener {
+public class QueueFragment extends Fragment implements OnClickListener,OnItemClickListener,OnScrollListener{
 	private ImageView dropDownLocation;
-	private ImageView placeChoice;
-	private ImageView teachType;
-	private ImageView nearPlace;
 	private LinearLayout orderLearn;
 	private LinearLayout queueWait;
 	private LinearLayout appItroduce;
@@ -42,9 +54,19 @@ public class QueueFragment extends Fragment implements OnClickListener {
 	private LinearLayout aboutUsCall;
 	private ListView listView;
 	private Context context;
-	private List<SecPageItemBean> list;
+	private Vector<SecPageItemBean> list;
 	private SecPageAdapter adapter;
 	private Activity mainActivity;
+	private Spinner spinnerPlaceChoice;
+	private Spinner spinnerTeachType;
+	private Spinner spinnerNearPlace;
+	private List<String> listPlaceChoice;
+	private List<String> listTeachType;
+	private List<String> listNearPlace;
+	private ArrayAdapter<String> adapterPlaceChoice;
+	private ArrayAdapter<String> adapterTeachType;
+	private ArrayAdapter<String> adapterNearPlace;
+	private ScrollView secScrollView;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -53,12 +75,8 @@ public class QueueFragment extends Fragment implements OnClickListener {
 
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		mainActivity=getActivity();
-		//View view = inflater.inflate(R.layout.queue, container, false);
+		mainActivity = getActivity();
 		dropDownLocation = (ImageView) mainActivity.findViewById(R.id.id_down_queue);
-		placeChoice = (ImageView) mainActivity.findViewById(R.id.id_place_choice);
-		teachType = (ImageView) mainActivity.findViewById(R.id.id_teach_type);
-		nearPlace = (ImageView) mainActivity.findViewById(R.id.id_near_place);
 		orderLearn = (LinearLayout) mainActivity.findViewById(R.id.id_order_learn);
 		queueWait = (LinearLayout) mainActivity.findViewById(R.id.id_queue_wait);
 		appItroduce = (LinearLayout) mainActivity.findViewById(R.id.id_app_indroduce);
@@ -67,35 +85,131 @@ public class QueueFragment extends Fragment implements OnClickListener {
 		feedbackPass = (LinearLayout) mainActivity.findViewById(R.id.id_feedback_pass);
 		aboutUsCall = (LinearLayout) mainActivity.findViewById(R.id.id_about_us_call);
 		listView = (ListView) mainActivity.findViewById(R.id.id_coach_self_choice);
+		spinnerPlaceChoice = (Spinner) mainActivity.findViewById(R.id.id_sp_place_choice);
+		spinnerTeachType = (Spinner) mainActivity.findViewById(R.id.id_sp_teach_type);
+		spinnerNearPlace = (Spinner) mainActivity.findViewById(R.id.id_sp_near_place);
+		secScrollView = (ScrollView) mainActivity.findViewById(R.id.id_queue_scrollview);
+		//添加底部视图
+		View footer =getLayoutInflater(savedInstanceState).inflate(R.layout.load_more,null);
+		listView.addFooterView(footer);
+		
 	}
-	
+
+
+
 	@Override
 	public void onStart() {
 		super.onStart();
-		
+		// 事件初始化
 		initEvent();
-
-		list=new ArrayList<SecPageItemBean>();
+	
+		//vector是线程安全的
+		list = new Vector<SecPageItemBean>();
+		// 教练列表
+		coachList();
 		
-		// List<SecPageItemBean> dataList = new ArrayList<>();
-		
-		for (int i = 0; i < 20; i++) {
-			list.add(new SecPageItemBean(R.drawable.coach, 3000+(i+1)*10, "小车",i+1, "查看更多" ));
-		}
-//		if(adapter==null){
-//			adapter = new SecPageAdapter(mainActivity,list);
-//		}
-		
-		listView.setAdapter(new SecPageAdapter(mainActivity,list));
-		// return view;
+		// 自选教练
+		choiceCochSelf();
 	}
 
+	// 教练列表
+	private void coachList() {
+
+		secScrollView.smoothScrollBy(0, 0);
+		initData();
+		//new LoadDataThread().start();//加载数据的工作线程
+		adapter=new SecPageAdapter(mainActivity, list);
+		listView.setAdapter(adapter);
+		// return view;
+		setListViewHeightBasedOnChildren(listView);
+	}
+
+	/**
+	 * 为listView添加数据
+	 */
+	int index=1; //数据的计数器(索引)
+	//初始化数据
+	 void initData() {
+		 
+		for (int i = 0; i < 10; i++) {
+			SecPageItemBean secItemBean = new SecPageItemBean();
+			secItemBean.coachPrice=3000 + (index) * 10;
+			secItemBean.coachSelfImg=R.drawable.coach;
+			secItemBean.teachType="小车";
+			secItemBean.orderTimes=index;
+			secItemBean.moreInfo="查看更多";
+			//list.add(new SecPageItemBean(R.drawable.coach, 3000 + (index) * 10, "小车", index, "查看更多"));
+			list.add(secItemBean);
+			index++;
+		}
+
+	}
+
+	// 获取并设置ListView高度的方法
+	public void setListViewHeightBasedOnChildren(ListView listView) {
+		ListAdapter listAdapter = listView.getAdapter();
+		if (listAdapter == null) {
+			return;
+		}
+
+		int totalHeight = 0;
+		for (int i = 0; i < listAdapter.getCount(); i++) {
+			View listItem = listAdapter.getView(i, null, listView);
+			listItem.measure(0, 0);
+			totalHeight += listItem.getMeasuredHeight();
+		}
+
+		ViewGroup.LayoutParams params = listView.getLayoutParams();
+		params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+		((MarginLayoutParams) params).setMargins(10, 10, 10, 10);
+		listView.setLayoutParams(params);
+	}
+
+	// 根据条件自选教练选项
+	private void choiceCochSelf() {
+
+		listPlaceChoice = new ArrayList<>();
+		listTeachType = new ArrayList<>();
+		listNearPlace = new ArrayList<>();
+
+		listPlaceChoice.add("武汉光谷广场");
+		listPlaceChoice.add("洪山森林公园");
+		listPlaceChoice.add("汉口楚河汉街");
+		listPlaceChoice.add("武昌街道口");
+		listPlaceChoice.add("武汉黄鹤楼");
+
+		listTeachType.add("小车C1");
+		listTeachType.add("小车C2");
+		listTeachType.add("中车B1");
+		listTeachType.add("中车B2");
+		listTeachType.add("大车A1");
+		listTeachType.add("大车A2");
+
+		listNearPlace.add("1-2km");
+		listNearPlace.add("2-4km");
+		listNearPlace.add("4-6km");
+		listNearPlace.add("6-8km");
+		listNearPlace.add("8-10km");
+
+		adapterPlaceChoice = new ArrayAdapter<String>(mainActivity, android.R.layout.simple_spinner_item,
+				listPlaceChoice);
+		adapterTeachType = new ArrayAdapter<String>(mainActivity, android.R.layout.simple_spinner_item, listTeachType);
+		adapterNearPlace = new ArrayAdapter<String>(mainActivity, android.R.layout.simple_spinner_item, listNearPlace);
+
+		adapterPlaceChoice.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		adapterTeachType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		adapterNearPlace.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		spinnerPlaceChoice.setAdapter(adapterPlaceChoice);
+		spinnerTeachType.setAdapter(adapterTeachType);
+		spinnerNearPlace.setAdapter(adapterNearPlace);
+
+	}
+
+	// 初始化监听事件
 	private void initEvent() {
 
 		dropDownLocation.setOnClickListener(this);
-		placeChoice.setOnClickListener(this);
-		teachType.setOnClickListener(this);
-		nearPlace.setOnClickListener(this);
 		orderLearn.setOnClickListener(this);
 		queueWait.setOnClickListener(this);
 		appItroduce.setOnClickListener(this);
@@ -103,6 +217,7 @@ public class QueueFragment extends Fragment implements OnClickListener {
 		guaranteeService.setOnClickListener(this);
 		feedbackPass.setOnClickListener(this);
 		aboutUsCall.setOnClickListener(this);
+		listView.setOnScrollListener(this);
 	}
 
 	@Override
@@ -113,21 +228,23 @@ public class QueueFragment extends Fragment implements OnClickListener {
 
 			Toast.makeText(mainActivity, "排队", Toast.LENGTH_SHORT).show();
 			break;
-		case R.id.id_place_choice:
-
-			Toast.makeText(mainActivity, "选择场地", Toast.LENGTH_SHORT).show();
-			break;
-		case R.id.id_teach_type:
-
-			Toast.makeText(mainActivity, "教学类型", Toast.LENGTH_SHORT).show();
-			break;
-		case R.id.id_near_place:
-
-			Toast.makeText(mainActivity, "附近场地", Toast.LENGTH_SHORT).show();
-			break;
+		// case R.id.id_place_choice:
+		//
+		// Toast.makeText(mainActivity, "选择场地", Toast.LENGTH_SHORT).show();
+		// break;
+		// case R.id.id_teach_type:
+		//
+		// Toast.makeText(mainActivity, "教学类型", Toast.LENGTH_SHORT).show();
+		// break;
+		// case R.id.id_near_place:
+		//
+		// Toast.makeText(mainActivity, "附近场地", Toast.LENGTH_SHORT).show();
+		// break;
 		case R.id.id_order_learn:
 
-			Toast.makeText(mainActivity, "学习次数", Toast.LENGTH_SHORT).show();
+			Intent intent1 = new Intent(mainActivity, OrderLearn.class);
+			startActivity(intent1);
+			// Toast.makeText(mainActivity, "学习次数", Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.id_queue_wait:
 
@@ -153,6 +270,66 @@ public class QueueFragment extends Fragment implements OnClickListener {
 
 			Toast.makeText(mainActivity, "联系我们", Toast.LENGTH_SHORT).show();
 			break;
+		}
+	}
+
+	/**
+	 * listView内容的点击事件
+	 * 
+	 */
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * listView里的滚动方法
+	 * 
+	 */
+	int visibleLastIndex=0;
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		
+		//System.out.println("totalItemCount:"+totalItemCount);
+		visibleLastIndex=firstVisibleItem+visibleItemCount-1;
+		
+	}
+
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		//adapter.getCount();
+		if(visibleLastIndex==adapter.getCount()&&scrollState==OnScrollListener.SCROLL_STATE_IDLE){
+			new LoadDataThread().start();
+		}
+	}
+	
+	//线程之间通信的桥梁handler
+	private Handler handler =new Handler(){
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			switch (msg.what) {
+			case 1:
+				adapter.notifyDataSetChanged();
+				break;
+			default:
+				break;
+			}
+		};
+	};
+
+	//模拟加载数据
+	class LoadDataThread extends Thread{
+		@Override
+		public void run() {
+			initData();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			//通过handler处理器去通知主线程，说数据已加载完毕
+			handler.sendEmptyMessage(1);
 		}
 	}
 
